6e4b530e92deba2f0cfd8271a042996db67c4540
Julian <juliannandres05@gmail.com>
2026-05-16 13:45:47 -0500

    Se crea la estructura inicial para poder levantar el servidor

---
+++ server/src/app.ts
@@ -0,0 +1,29 @@
+// -------Impotando modulos y librearias
+import express from "express"
+import dotenv from "dotenv"
+import cors from 'cors'
+dotenv.config();
+const app = express()
+
+// -------Importando rutas
+
+
+// ------- Settings de nuestro backend
+app.set('case sensitive Routing', true)
+app.set('appName', 'Express app')
+app.set('port', process.env.PORT) // -----TRAER EL PUERTO CON UNA VARIBALE DE ENTORNO
+
+
+// ------- MIDDLEWARES ------
+app.use(cors( {
+    origin: "http://localhost:5173"
+} ))
+app.use(express.json())
+app.use(express.urlencoded( {extended: false} ))
+
+
+// ------- RUTAS CREADAS -----
+
+
+
+export default app
