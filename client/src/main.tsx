e8847cdb7174a9b2d3a307c875e90df7b02a322d
Julian <juliannandres05@gmail.com>
2026-05-27 13:00:14 -0500

    Revert "Merge pull request #16 from aenami/notas-y-faltas-sprint-2"
    This reverts commit 90470bf43a4cc993597a104b04a7c0ff1e403ba3, reversing
    changes made to 7e6fa908232f6561b379af59152b4375e08aa9b6.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
@@ -21,18 +20,3 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(<RouterProvider router={router} />)
 }
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
->>>>>>> 8523957b7318815d7b2adaf222136bcc3979e159
--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,6 @@ import authRoutes from './routes/auth.routes.js'
 import groupRoutes from './routes/grupo.routes.js'
 import corteRoutes from './routes/corte.routes.js'
 import studentsRoutes from './routes/estudiantes.routes.js'
-import notaRoutes from './routes/nota.routes.js' 
 import usersRoutes from './routes/users.routes.js'
 
 
@@ -36,7 +35,6 @@ app.use('/api/groups', groupRoutes)
 app.use('/api/cortes', corteRoutes)
 
 app.use('/api/grupos', studentsRoutes)
-app.use('/api/notas', notaRoutes)
 app.use('/api/users', usersRoutes)
 
 export default app
